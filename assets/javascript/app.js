c9177ccbd0e53a1672fd7a4288dde273c0b84b85
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-07-12 15:22:22 -0400

    optimized function calls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,8 @@
-let buttons = ['hamster', 'hawk', 'dolphin'];
+let buttons = ['hamster', 'hawk', 'dolphin']; 
 
-let toDoCount = 0;  
-
-let createButtons = () => {
+const createButtons = () => {
     $('#buttons').empty();
     buttons.forEach(value => {
-        console.log(value);
         let button = `<button class='gifBtn' data-value='${value}'>${value}`;
         $('#buttons').append(button);
     });
@@ -22,68 +19,35 @@ function getGif() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
+            
             for (let i = 0; i < 10; i++) {
                 let stillURL = response.data[i].images.original_still.url;
-                let animateURL = response.data[i].images.original.webp;
-
-                console.log(stillURL);
+                let animateURL = response.data[i].images.original.url;
                 let gif = $('<img>');
                 gif.attr('class', 'gif').attr('src', stillURL).attr('data-animate', animateURL).attr('data-still', stillURL).attr('data-state', 'still');
                 $('#gifs').prepend(gif);
             }
 
-            console.log('here');
-    $(".gif").on("click", function() {
-        // STEP ONE: study the html above.
-        // Look at all the data attributes.
-        // Run the file in the browser. Look at the images.
-    
-        // After we complete steps 1 and 2 we'll be able to pause gifs from giphy.
-    
-        // STEP TWO: make a variable named state and then store the image's data-state into it.
-        // Use the .attr() method for this.
-        let state = $(this).attr('data-state');
-        console.log(state);
-        // ============== FILL IN CODE HERE FOR STEP TWO =========================
-    
-        // CODE GOES HERE
-    
-        // =============================================
-    
-        // STEP THREE: Check if the variable state is equal to 'still',
-        // then update the src attribute of this image to it's data-animate value,
-        // and update the data-state attribute to 'animate'.
-        if (state === 'still') {
-          $(this).attr('data-state', 'animate');
-          $(this).attr('src', $(this).attr('data-animate'));
-        } else {
-          $(this).attr('data-state', 'still');
-          $(this).attr('src', $(this).attr('data-still'));
-        }
-        // If state is equal to 'animate', then update the src attribute of this
-        // image to it's data-still value and update the data-state attribute to 'still'
-        // ============== FILL IN CODE HERE FOR STEP THREE =========================
-    
-        // CODE GOES HERE
-    
-        // ==============================================
-    
-        // STEP FOUR: open the file in the browser and click on the images.
-        // Then click again to pause.
-      });
+            $(".gif").on("click", function() {
+              let state = $(this).attr('data-state');
+              if (state === 'still') {
+                $(this).attr('data-state', 'animate');
+                $(this).attr('src', $(this).attr('data-animate'));
+              } else {
+                $(this).attr('data-state', 'still');
+                $(this).attr('src', $(this).attr('data-still'));
+              }
+            });
         });
     });
     
 }
 
 function addBtn() {
-  $("#addQuery").on("click", function(event) {
+  $("#addBtn").on("click", function(event) {
     event.preventDefault();
-    console.log(event);
     let newQuery = $("#newQuery").val().trim();
     buttons.push(newQuery);
-    console.table(buttons);
     document.getElementById('form').reset();
     createButtons();
   });
